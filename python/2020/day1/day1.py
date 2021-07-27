18f983a071cb1e21da9449e8caac39b5f69b676f
imry <imry.rosnebaum@gmail.com>
2021-07-27 22:30:08 +0100

    First Day Solution

---
+++ python/2020/day1/day1.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import os as os
+
+file = 'input.txt'
+
+report= {}
+
+if __name__=="__main__":
+    #part 1
+    with open(file, 'r') as reader:
+        line = reader.readline()
+        while line != '':
+            if report.get(2020-int(line)):
+                print("Part1")
+                print(int(line)*(2020-int(line)))
+            report[int(line)] = report.get(int(line),0) + 1
+            line = reader.readline()
+
+    keys = list(map(int,report.keys()))
+
+    for i in range(len(keys)):
+        for j in range(i,len(keys)):
+           for k in range(j,len(keys)):
+               if (i !=j) and (k != j):
+                   if (int(keys[i])+int(keys[j])+int(keys[k]))==2020:
+                       print("Part 2")
+                       print(keys[i]*keys[j]*keys[k])
+
+    print("Le Fin")
